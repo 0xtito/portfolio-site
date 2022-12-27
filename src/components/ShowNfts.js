d1a879c7747a0c1cc36c3f7d126eee55a1cbd960
0xtito <welovethepenguins@gmail.com>
2022-12-27 02:55:59 -0500

    cleaned up components

--- src/components/ShowNfts.js
+++ src/components/ShowNfts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const defaultImage = require("../../images/default_image.png").default;
 
@@ -7,21 +7,22 @@ let resNfts, gifs, resImg;
 let gifsArr, finalGifs;
 
 function ShowNfts() {
-  const [name, setName] = React.useState(null);
-  const [contractAddress, setContractAddress] = React.useState(null);
-  const [image, setImage] = React.useState(null);
-  const [gif, setGifs] = React.useState(null);
-  const [animation, setAnimation] = React.useState(null);
-
+  const [name, setName] = useState(null);
+  const [contractAddress, setContractAddress] = useState(null);
+  const [image, setImage] = useState(null);
+  const [gif, setGifs] = useState(null);
+  const [animation, setAnimation] = useState(null);
+  const [caption, setCaption] = useState(null);
+  const curCaption = useRef();
 
   let url = "http://localhost:3001/api";
   let header = new Headers({
     "Access-Control-Allow-Origin": "*",
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     let nfts;
-    let curNft = 0
+    let curNft = 0;
     async function fetchData() {
       try {
         resNfts = await fetch(`${url}/nfts`, {
@@ -30,32 +31,32 @@ function ShowNfts() {
         let { nfts: nftsObj, orderNfts } = await resNfts.json();
 
         gifs = await fetch(`${url}/gifs`, {
-          method: 'GET'
-        })
-        gifs = await gifs.json()
-        console.log(gifs)
-        
-        let gifPromises = gifs.map( async (obj) => {
-          return fetch(`${url}/gifs/${obj.name}`)
+          method: "GET",
+        });
+        gifs = await gifs.json();
+        console.log(gifs);
+
+        let gifPromises = gifs.map(async (obj) => {
+          return fetch(`${url}/gifs/${obj.name}`);
           // return [await fetch(`${url}/gifs/${obj.name}`), obj]
         });
-        let resGifsUrls = await Promise.all(gifPromises)
+        let resGifsUrls = await Promise.all(gifPromises);
 
         // put the urls in resGifsUrls into resGifNames
         gifs.forEach((obj, i) => {
-          obj.url = resGifsUrls[i].url
-        })
+          obj.url = resGifsUrls[i].url;
+        });
 
         nfts = filterNfts(nftsObj, orderNfts);
 
         setName(nfts[curNft].name);
-        setImage(nfts[curNft].metadata.image)
+        setImage(nfts[curNft].metadata.image);
+        curCaption.current = name;
 
         setTimeout(() => {
-          curNft++
-          showNfts(nfts)
+          curNft++;
+          showNfts(nfts);
         }, 3000);
-
       } catch (error) {
         console.error(error);
       }
@@ -69,27 +70,55 @@ function ShowNfts() {
       }
 
       setName(nfts[curNft].name);
+      curCaption.current = name;
       if (nfts[curNft].isAnimated) {
-        pauseTime = 5000
-        setImage(nfts[curNft].metadata.animation_url)
+        pauseTime = 5000;
+        setImage(nfts[curNft].metadata.animation_url);
       } else {
-        setImage(nfts[curNft].metadata.image)
+        setImage(nfts[curNft].metadata.image);
       }
 
-      curNft++
-      setTimeout( () => {
-        showNfts(nfts)
+      curNft++;
+      setTimeout(() => {
+        showNfts(nfts);
       }, pauseTime);
-
     }
   }, []);
 
   return (
     <div>
       {/* <a> */}
-        <img className="nft-image" src={image}></img>
+      <img
+        className="nft-image"
+        src={image}
+        onMouseOver={(e) => {
+          let image = e.currentTarget;
+          let parent = image.parentElement;
+          let children = parent.children;
+          for (let i = 0; i < children.length; i++) {
+            let child = children[i];
+            if (child.classList.contains("hide-caption")) {
+              child.classList.replace("hide-caption", "show-caption");
+              // setName(name);
+            }
+          }
+        }}
+        onMouseLeave={(e) => {
+          let image = e.currentTarget;
+          let parent = image.parentElement;
+          // let children = parent.childNodes;
+          let children = parent.children;
+          for (let i = 0; i < children.length; i++) {
+            console.log(child);
+            let child = children[i];
+            if (child.classList.contains("show-caption")) {
+              child.classList.replace("show-caption", "hide-caption");
+            }
+          }
+        }}
+      ></img>
       {/* </a> */}
-      <p className="image-title">{!name ? "Loading..." : name}</p>
+      <p className="image-title hide-caption">{!name ? "" : name}</p>
     </div>
   );
 }
@@ -108,16 +137,16 @@ function filterNfts(nftsObj, isOrdered) {
   }
   // give the NFTs the correct links
   for (let nft of rawNftsArr) {
-    if(!nft.isAnimated) {
-      if (nft.metadata.image.startsWith('ipfs')) {
-        let key = nft.metadata.image.split('//')[1]
-        nft.metadata.image = `https://ipfs.io/ipfs/${key}`
+    if (!nft.isAnimated) {
+      if (nft.metadata.image.startsWith("ipfs")) {
+        let key = nft.metadata.image.split("//")[1];
+        nft.metadata.image = `https://ipfs.io/ipfs/${key}`;
       }
     } else {
-      console.log(gifsArr)
-      if (gifs.some( (info) => info.id == nft.id)) {
-        let el = gifs.find( ((info) => info.id == nft.id))
-        console.log(el)
+      console.log(gifsArr);
+      if (gifs.some((info) => info.id == nft.id)) {
+        let el = gifs.find((info) => info.id == nft.id);
+        console.log(el);
         nft.metadata.animation_url = el.url;
       }
     }
