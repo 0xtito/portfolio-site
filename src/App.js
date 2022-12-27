5283a08a4353a8f78168a0f43e9599e14fc753cd
0xtito <welovethepenguins@gmail.com>
2022-12-27 02:56:35 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import ChooseHeader from "./components/ChooseHeader.js";
+import ChooseFromList from "./components/ChooseFromList.js";
 import ShowNfts from "./components/ShowNfts.js";
 // import NftScroller from "./components/NftScroller.js";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // function App() {
 //   return (
@@ -17,11 +17,16 @@ import React from "react";
 //   );
 // }
 
+let eth = ".eth";
+
 function App() {
+  const { jsx, backJsx, description } = ChooseFromList();
+  const [eth, setEth] = useState(null);
+
   return (
     <div>
       <div className="content-container content-grid">
-        <div grid-column="1"></div>
+        {/* <div grid-column="1"></div> */}
         <div className="intro_content content_width">
           I grew up in South Florida playing sports and always being with
           friends. I went to Saint Andrew’s High School for academics and
@@ -35,13 +40,36 @@ function App() {
           Now I spend my days coding, teaching children how to code, and
           learning all I can about whatever interests me.
         </div>
-        <div className="list">
-          <ChooseHeader />
+        <div className="image-container">
+          <ShowNfts />
         </div>
-      </div>
-      <div id="content-container image-container">
-        <div className=""></div>
-        <ShowNfts />
+        <div className="title-container">
+          <div
+            className="title"
+            onMouseOver={(e) => {
+              let title = e.currentTarget;
+              setEth(".eth");
+            }}
+            onMouseLeave={(e) => {
+              let title = e.currentTarget;
+              // title.classList.add("show-eth");
+              setEth("");
+            }}
+            onClick={() =>
+              window.open(
+                "https://app.ens.domains/name/0xtito.eth/details",
+                "_blank"
+              )
+            }
+          >
+            0xtito<span className="show-eth">{eth}</span>
+          </div>
+        </div>
+        <div className="list-container">{jsx}</div>
+        <div className="return-container">{backJsx}</div>
+        <div className="description-container">
+          <p className="description">{description}</p>
+        </div>
       </div>
     </div>
   );
