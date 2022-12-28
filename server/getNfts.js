ecfce70b52c4b1906077e00c21900c66ab4dd719
0xtito <welovethepenguins@gmail.com>
2022-12-28 01:13:31 -0500

    updated server scripts

--- server/getNfts.js
+++ server/getNfts.js
@@ -1,24 +1,19 @@
-const ethers = require('ethers');
+const ethers = require("ethers");
 // import app from "./server.js"
-const { Network, Alchemy } = require('alchemy-sdk');
-const dotenv = require('dotenv');
+const { Network, Alchemy } = require("alchemy-sdk");
+const dotenv = require("dotenv");
 dotenv.config();
 
-
-const address = "0xD2128b1C22Bb80a4dae69fe149cD6fE9Ba7eB4aa"
-const url = process.env.MAINNET_URL;
-const apiKey = process.env.API_KEY
+const address = "0xD2128b1C22Bb80a4dae69fe149cD6fE9Ba7eB4aa";
+const apiKey = process.env.API_KEY;
 
 const settings = {
   apiKey: apiKey,
-  Network: Network.ETH_MAINNET
-}
+  Network: Network.ETH_MAINNET,
+};
 
 const alchemy = new Alchemy(settings);
 
 module.exports = async function getNfts() {
-
-
   return await alchemy.nft.getNftsForOwner(address);
-
-}
+};
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 let getNfts = require("./getNfts.js");
-const path = require('path')
+const path = require("path");
 
 // console.log(kafka_gif)
 
@@ -49,52 +49,110 @@ app.use(cors(corsOptions));
     "Kafka #468",
     "Galaxy Egg #9667",
     "Galaxy Egg #1002",
-  ]
+  ];
+
+  let pudgy;
 
   for (let i = 0; i < totalNfts; i++) {
     let nft = ownedNfts[i];
 
+    if (nft.title == "Lil Pudgy #4495") {
+      pudgy = nft;
+    }
     if (
       unwantedNfts.includes(nft.contract.name) ||
       unwantedNfts.includes(nft.name) ||
       unwantedNfts.includes(nft.title) ||
       nft.contract.address == ensAddress
-    ) continue;
+    )
+      continue;
 
     nfts[`nft${i}`] = {
       name: nft.title,
       id: i,
-      order: orderByTitle.includes(nft.title) ? orderByTitle.indexOf(nft.title) : -1,
+      order: orderByTitle.includes(nft.title)
+        ? orderByTitle.indexOf(nft.title)
+        : -1,
       isAnimated: nft.rawMetadata.animation_url ? true : false,
       contract: nft.contract,
       metadata: nft.rawMetadata,
     };
   }
 
-  // nfts.nft28.metadata.animation_url =
-
-  // console.log(path.join(__dirname, 'atlanta.gif'))
-
   app.get("/api/nfts", (req, res) => {
-    res.json({nfts, orderNfts});
+    res.json({ nfts, orderNfts });
+  });
+
+  app.get("/api/nfts/pudgy", (req, res) => {
+    res.json(pudgy);
   });
 
   app.get("/api/gifs", (req, res) => {
     let imageIds = [];
 
-    Object.values(nfts).forEach( (info) => {
+    Object.values(nfts).forEach((info) => {
       if (info.isAnimated) {
-        imageIds.push({name: info.name.split(' ')[0].toLowerCase(), id: info.id})
+        imageIds.push({
+          name: info.name.split(" ")[0].toLowerCase(),
+          id: info.id,
+        });
       }
-    })
+    });
 
     res.send(imageIds);
   });
 
-  app.get("/api/gifs/:id", (req,res) => {
-    const filePath = path.join(process.cwd(), 'images', 'gifs', `${req.params.id}.gif`)
-    res.sendFile(filePath)
-  })
+  app.get("/api/text/description-text", (req, res) => {
+    const textMap = {
+      past: {
+        text: 'This is the text for the "past" list item. This is the text for the "past" list item.This is the text for the "past" list item.This is the text for the "past" list item.This is the text for the "past" list item.This is the text for the "past" list item.This is the text for the "past" list item.',
+        isClicked: false,
+      },
+      current: {
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis bibendum ut tristique et egestas quis ipsum suspendisse. Mattis enim ut tellus elementum sagittis vitae et. Posuere ac ut consequat semper viverra nam. Purus faucibus ornare suspendisse sed nisi lacus sed viverra. Posuere ac ut consequat semper viverra nam libero justo laoreet. Praesent tristique magna sit amet purus. \nSed odio morbi quis commodo odio aenean sed. Orci sagittis eu volutpat odio facilisis mauris sit amet. Odio ut sem nulla pharetra diam sit. Pharetra magna ac placerat vestibulum lectus mauris ultrices. \n\n\nFaucibus vitae aliquet nec ullamcorper sit amet risus nullam. Eget mi proin sed libero. Tincidunt nunc pulvinar sapien et ligula. Est placerat in egestas erat imperdiet sed. Arcu non sodales neque sodales ut.",
+        isClicked: false,
+      },
+      future: {
+        text: 'This is the text for the "future" list item.',
+        isClicked: false,
+      },
+      interests: {
+        text: 'This is the text for the "interests" list item.',
+        isClicked: false,
+      },
+    };
+
+    res.json(textMap);
+  });
+
+  app.get("/api/text/intro", (req, res) => {
+    const intro =
+      "I grew up in South Florida playing sports and always being with friends. I went to Saint Andrew’s High School for academics and lacrosse. After an amazing 4 years, I went to Lehigh University to study Mechanical Engineering, however, two years in, I realized and accepted that I had no idea what I wanted to do with my life, but becoming a mechanical engineer and growing deeper in debt was the only thing I was certain I was not going to do. After leaving, I spent the next few years soul searching between jobs and hobbies, and ultimately ended up entangled in the world of crypto and software engineering. Now I spend my days coding, teaching children how to code, and learning all I can about whatever interests me.";
+    res.json(intro);
+  });
+
+  app.get("/api/text/title-content", (req, res) => {
+    const content = {
+      name: "0xtito",
+      nameWithEth: "0xtito.eth",
+    };
+    res.json(content);
+  });
+
+  app.get("/api/phrase", (req, res) => {
+    const phrase = '"not today"';
+    res.json(phrase);
+  });
+
+  app.get("/api/gifs/:id", (req, res) => {
+    const filePath = path.join(
+      process.cwd(),
+      "images",
+      "gifs",
+      `${req.params.id}.gif`
+    );
+    res.sendFile(filePath);
+  });
 });
 
 app.listen(PORT, () => {
