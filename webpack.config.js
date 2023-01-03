b24f5e3d69c4968091d1ded53f7faf629e43cbd6
0xtito <92818759+0xtito@users.noreply.github.com>
2023-01-02 23:13:27 -0500

    removed webpack for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,53 @@
-const path = require("path");
-const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
+// const path = require("path");
+// const webpack = require("webpack");
+// const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve("dist/js"),
-    publicPath: "/dist",
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [new Dotenv()],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.(png|gif)$/,
-        use: {
-          loader: "file-loader",
-        },
-      },
-      {
-        test: /\.gif$/,
-        use: {
-          loader: "file-loader",
-        },
-      },
-    ],
-  },
-};
+// module.exports = {
+//   entry: "./src/index.js",
+//   mode: "development",
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve("dist/js"),
+//     publicPath: "/dist",
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//   },
+//   plugins: [new Dotenv()],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: "babel-loader",
+//       },
+//       {
+//         test: /\.css$/,
+//         exclude: /node_modules/,
+//         use: ["style-loader", "css-loader"],
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader",
+//           options: {
+//             presets: ["@babel/preset-react"],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.(png|gif)$/,
+//         use: {
+//           loader: "file-loader",
+//         },
+//       },
+//       {
+//         test: /\.gif$/,
+//         use: {
+//           loader: "file-loader",
+//         },
+//       },
+//     ],
+//   },
+// };
