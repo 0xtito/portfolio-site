22c602eaf91b84326fe32a59f18b901f31d94f4e
0xtito <welovethepenguins@gmail.com>
2023-01-12 11:54:50 -0500

    nextjs config

---
+++ next.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  httpAgentOptions: {
+    keepAlive: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        port: "",
+        pathname: "/116006098.png",
+        hostname: "media.artblocks.io",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname:
+          "/ipfs/QmZfGmXqVwAEaEWMnCmMLowjCdN9QJPk79SbXq3gmk1VfL/9667.jpg",
+        hostname: "ipfs.io",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname: "/ipfs/QmYHkHqChRBrBrCAfz3MLs2fh3i5jbprSaRRegahUkkY19",
+        hostname: "ipfs.io",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname:
+          "/ipfs/QmZfGmXqVwAEaEWMnCmMLowjCdN9QJPk79SbXq3gmk1VfL/1002.jpg",
+        hostname: "ipfs.io",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname: "/ipfs/QmfM8it9QHW3dKQFwZ8Zisn6ZbDBmYg44ooKCL17RhBQyS",
+        hostname: "ipfs.io",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname: "/api/image/1059",
+        hostname: "kinesis.art",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname: "/api/image/1000468",
+        hostname: "kinesis.art",
+      },
+      {
+        protocol: "https",
+        port: "",
+        pathname:
+          "/DvMZwhyaIKnfOPy1ucG8ygcEb5GgaYSUl3kNF9UqExNjGcBzH3pKcv9Xlr2882-7N-nWWaIwy45JP28FgmRKvNrs8JVSV1QBxYeW",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        port: "",
+        hostname: "github.com",
+        pathname:
+          "/0xtito/portfolio-site/blob/master/images/gifs/atlanta.gif?raw=true",
+      },
+    ],
+  },
+};
