f2dcba1f865160d00012e0c6462a83e75a5fc981
0xtito <welovethepenguins@gmail.com>
2023-01-27 10:27:18 -0500

    restructured

--- src/components/contexts/NftsContext.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const NftsContext = createContext([]);
-
-export const Provider = NftsContext.Provider;
-export const Consumer = NftsContext.Consumer;
