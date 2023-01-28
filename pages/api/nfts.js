8a1589107e02688a317e9c4ad3660aac74113935
0xtito <welovethepenguins@gmail.com>
2023-01-27 19:30:06 -0500

    updating api

--- pages/api/nfts.js
+++ pages/api/nfts.js
@@ -1,12 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { get } from "@vercel/edge-config";
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
-export default async function () {
-  const descriptions = await get("nfts");
+export default async function (req, res) {
+  // const images = await get("images");
+  try {
+    const images = await get("images");
+    return res.json(images);
+  } catch (err) {
+    return res.json({ message: err.message });
+  }
 
-  return NextResponse.json(descriptions);
+  // return NextResponse.json(images);
 }
