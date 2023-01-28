c56e4f558aa36c85db406dce843ebd96a4f97cbf
0xtito <welovethepenguins@gmail.com>
2023-01-27 19:38:12 -0500

    test new api

--- pages/api/content.js
+++ pages/api/content.js
@@ -1,22 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { get } from "@vercel/edge-config";
 
-export const config = {
-  runtime: "edge",
-};
+// export const config = {
+//   runtime: "edge",
+// };
 
-export default async function () {
-  const descriptions = await get("descriptions");
-  const intro = await get("intro");
-
-  const body = {
-    descriptions,
-    intro,
-  };
-
-  return NextResponse.json({ descriptions, intro });
+export default async function (req, res) {
+  try {
+    const descriptions = await get("descriptions");
+    const intro = await get("intro");
+    return res.status(200).json({ descriptions, intro });
+  } catch (err) {
+    return res.status.json({ message: err.message });
+  }
 }
 
-// export default async function handler(req) {
-//   return new Response()
+// export default async function () {
+//   const descriptions = await get("descriptions");
+//   const intro = await get("intro");
+
+//   const body = {
+//     descriptions,
+//     intro,
+//   };
+
+//   return NextResponse.json({ descriptions, intro });
 // }
