86dbdbe3f1be30d7b6962e73abf5c37850f6c9c1
0xtito <welovethepenguins@gmail.com>
2023-01-27 20:01:01 -0500

    KISS

--- pages/index.js
+++ pages/index.js
@@ -13,54 +13,29 @@ function MainPage({ titles }) {
 }
 
 export async function getStaticProps() {
-  const _apiUrl = process.env.VERCEL_ENV
-    ? process.env.VERCEL_URL
-    : "http://localhost:3000";
-  console.log(process.VERCEL_URL);
-  const apiUrl = "https://portfolio-site-13ngakkxa-0xtito.vercel.app/";
+  const descriptions = await get("descriptions");
+  const intro = await get("intro");
+  const { carousel, pudgy } = await get("images");
 
-  try {
-    const images = await fetch(`/api/nfts`, {
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-    const { carousel, pudgy } = await images.json();
-    console.log(pudgy);
-    const content = await fetch(`/api/content`, {
-      headers: {
-        "Content-type": "application/json",
-      },
-    });
-    const { descriptions, intro } = await content.json();
-    console.log(intro);
-
-    return {
-      props: {
-        descriptions: descriptions.map(({ description, title }) => ({
-          title,
-          description,
-        })),
-        titles: descriptions.map(({ title }) => title),
-        intro,
-        nfts: carousel.map((nft) => ({
-          title: nft.name,
-          isAnimated: nft.isAnimated,
-          imageUrl: !nft.isAnimated ? nft.image : null,
-          animationUrl: nft.isAnimated ? nft.animationUrl : null,
-        })),
-        defaultNft: carousel[0].image,
-        pudgyImg: pudgy.image,
-      },
-      revalidate: 1,
-    };
-  } catch (err) {
-    console.error(err.message);
-  }
-
-  // const descriptions = await get("descriptions");
-  // const intro = await get("intro");
-  // const { carousel, pudgy } = await get("images");
+  return {
+    props: {
+      descriptions: descriptions.map(({ description, title }) => ({
+        title,
+        description,
+      })),
+      titles: descriptions.map(({ title }) => title),
+      intro,
+      nfts: carousel.map((nft) => ({
+        title: nft.name,
+        isAnimated: nft.isAnimated,
+        imageUrl: !nft.isAnimated ? nft.image : null,
+        animationUrl: nft.isAnimated ? nft.animationUrl : null,
+      })),
+      defaultNft: carousel[0].image,
+      pudgyImg: pudgy.image,
+    },
+    revalidate: 1,
+  };
 }
 
 export default MainPage;
