753338b8ec4ef3b766054d18b79d354eb67b588b
0xtito <welovethepenguins@gmail.com>
2023-01-27 21:51:14 -0500

    too. many. commits. HELP

--- components/Card.jsx
+++ components/Card.jsx
@@ -15,33 +15,6 @@ export default function Card({ nfts, defaultNft, pudgyImg, intro }) {
       <Head>
         <title>{activePage ? `0xtito - ${activePage}` : "0xtito - home"}</title>
         <meta name="0xtito" lang="en" content="portfolio site" />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a73666f (preview deployment test)
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest"></link>
-<<<<<<< HEAD
-=======
->>>>>>> d8e2226 (rearranged folder structure)
-=======
->>>>>>> a73666f (preview deployment test)
       </Head>
       <Image
         width={75}
